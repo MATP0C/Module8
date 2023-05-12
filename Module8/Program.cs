834cfd8e9be74ef077aebd01a54a8572bc98d2d7
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-05-12 12:03:26 +0300

    Добавьте файлы проекта.

---
+++ Module8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Module8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo("C:\\");
+                if (dirInfo.Exists)
+                {
+                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
